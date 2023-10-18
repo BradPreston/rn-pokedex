a4d3de942795982a76a2c87ec514c909dd7f7172
bradpreston <bap5393@gmail.com>
2023-10-18 08:04:57 -0400

    Update Pokemon screen with data from GraphQL query

--- app/(pokemon)/pokemon.tsx
+++ app/(pokemon)/pokemon.tsx
@@ -1,12 +1,27 @@
-import { Text, View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Link } from 'expo-router';
+import getAllPokemon from '../../graphql/getAllPokemon';
+import { useEffect, useState } from 'react';
+import { Pokemon } from '../../types';
 
 export default function AllPokemon() {
+	const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+	console.log(pokemon ?? pokemon);
+
+	useEffect(() => {
+		getAllPokemon().then((pkmn) => setPokemon(pkmn));
+	}, []);
+
 	return (
 		<View>
-			<Link href='/pokemon/1'>Pokemon #1</Link>
-			<Link href='/pokemon/2'>Pokemon #2</Link>
-			<Link href='/pokemon/3'>Pokemon #3</Link>
+			<FlatList
+				showsVerticalScrollIndicator={false}
+				data={pokemon}
+				renderItem={({ item }) => (
+					<Link href={`/pokemon/${item.id}`}>{item.name}</Link>
+				)}
+				keyExtractor={(item) => item.name}
+			/>
 		</View>
 	);
 }
