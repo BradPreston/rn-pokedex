d80192947921b2f9698a3ed7450656d523995aca
bradpreston <bap5393@gmail.com>
2023-10-21 14:17:40 -0400

    Update Pokemon type and add Species type

--- types/index.ts
+++ types/index.ts
@@ -2,4 +2,50 @@ export type Pokemon = {
 	__typename: string;
 	id: number;
 	name: string;
+	height: number;
+	weight: number;
+	abilities: {
+		nodes: Ability[];
+	};
+	stats: Stat[];
+	types: Type[];
+	pokemonSprites: Sprite[];
+};
+
+export type Species = {
+	name: string;
+	is_legendary: boolean;
+	is_mythical: boolean;
+	habitat: {
+		name: string;
+	};
+	flavorText: FlavorText[];
+};
+
+type Ability = {
+	ability: {
+		name: string;
+	};
+};
+
+type Stat = {
+	base_stat: number;
+	stat: {
+		name: string;
+	};
+};
+
+type Type = {
+	slot: number;
+	type: {
+		name: string;
+	};
+};
+
+type Sprite = {
+	sprites: string;
+};
+
+type FlavorText = {
+	flavor_text: string;
 };
