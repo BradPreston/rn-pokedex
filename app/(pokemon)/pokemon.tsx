efac6481f2eab7895f146fec2bb2d24eb08f9ff4
bradpreston <bap5393@gmail.com>
2023-10-21 14:51:54 -0400

    Update Pokemon FlatList to use ListItem

--- app/(pokemon)/pokemon.tsx
+++ app/(pokemon)/pokemon.tsx
@@ -1,27 +1,34 @@
-import { View, FlatList } from 'react-native';
-import { Link } from 'expo-router';
+import { View, FlatList, Image } from 'react-native';
 import getAllPokemon from '../../graphql/getAllPokemon';
 import { useEffect, useState } from 'react';
 import { Pokemon } from '../../types';
+import ListItem from '../../components/ListItem';
 
 export default function AllPokemon() {
 	const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-	console.log(pokemon ?? pokemon);
+	const [loaded, setLoaded] = useState(false);
 
 	useEffect(() => {
+		setLoaded(false);
 		getAllPokemon().then((pkmn) => setPokemon(pkmn));
+		pokemon.forEach((pkmn) =>
+			Image.prefetch(
+				`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pkmn.id}.png`
+			)
+		);
+		setLoaded(true);
 	}, []);
 
 	return (
 		<View>
-			<FlatList
-				showsVerticalScrollIndicator={false}
-				data={pokemon}
-				renderItem={({ item }) => (
-					<Link href={`/pokemon/${item.id}`}>{item.name}</Link>
-				)}
-				keyExtractor={(item) => item.name}
-			/>
+			{loaded && (
+				<FlatList
+					showsVerticalScrollIndicator={false}
+					data={pokemon}
+					renderItem={({ item }) => <ListItem id={item.id} name={item.name} />}
+					keyExtractor={(item) => item.name}
+				/>
+			)}
 		</View>
 	);
 }
