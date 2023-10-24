44dabeb3d9e9aca236bd2e77c46bb9835d3d275d
bradpreston <bap5393@gmail.com>
2023-10-24 07:31:23 -0400

    Update party to show party pokemon and a button to remove all from party

--- app/(pokemon)/party.tsx
+++ app/(pokemon)/party.tsx
@@ -1,13 +1,56 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Link, useFocusEffect } from 'expo-router';
+import { Text, View, FlatList } from 'react-native';
+import { useQuery } from 'react-query';
+import { getPokemonFromParty, removeAllPokemonFromParty } from '../../storage';
+import { useRef, useCallback } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export function useRefreshOnFocus<T>(refetch: () => Promise<T>) {
+	const firstTimeRef = useRef(true);
+
+	useFocusEffect(
+		useCallback(() => {
+			if (firstTimeRef.current) {
+				firstTimeRef.current = false;
+				return;
+			}
+
+			refetch();
+		}, [refetch])
+	);
+}
+
 export default function Party() {
-	return (
-		<SafeAreaProvider>
+	const { data, status, refetch } = useQuery(
+		'pokemonParty',
+		getPokemonFromParty
+	);
+	useRefreshOnFocus(refetch);
+
+	async function clearPokemonParty() {
+		await removeAllPokemonFromParty();
+		refetch();
+	}
+
+	if (status === 'success') {
+		return (
 			<View>
 				<Text>Your Party</Text>
-				<Link href='/(pokemon)/pokemon'>Add pokemon to your party</Link>
+				{/* {data.map((pkmn) => (
+					<Text key={pkmn.name}>{pkmn.name}</Text>
+				))} */}
+				<FlatList
+					data={data}
+					keyExtractor={(item) => item.name}
+					renderItem={({ item }) => <Text>{item.name}</Text>}
+				/>
+				<Link className='py-5' href='/(pokemon)/pokemon'>
+					Add pokemon to your party
+				</Link>
+				<TouchableOpacity onPress={clearPokemonParty}>
+					<Text>Remove All Pokemon From Party</Text>
+				</TouchableOpacity>
 			</View>
-		</SafeAreaProvider>
-	);
+		);
+	}
 }
