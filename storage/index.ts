552c598235dce195ffb21f19e72c3ab20b9be7bd
bradpreston <bap5393@gmail.com>
2023-10-24 19:27:40 -0400

    Update storage to use react-native-storage

--- storage/index.ts
+++ storage/index.ts
@@ -1,108 +1,95 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Species } from '../types';
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Party = {
 	[key: string]: Species;
 };
 
-export async function setParty(value: Species) {
-	try {
-		const party = await getParty();
-		if (party[value.name])
-			throw new ReferenceError(
-				`A pokemon with the name \"${value.name}\" already exists`
-			);
-		if ((await allowOnlySixInParty()) === false) return;
-		party[value.name] = value;
+function Party() {
+	const storage = new Storage({
+		size: 6,
+		storageBackend: AsyncStorage,
+		defaultExpires: null,
+		enableCache: true
+	});
 
-		await AsyncStorage.setItem('party', JSON.stringify(party));
-	} catch (err) {
-		console.error(err);
-	}
-}
-
-export async function getOnePartyPokemon(
-	key: string
-): Promise<Species | undefined> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party: Party = JSON.parse(partyJSON);
-		if (!party[key])
-			throw new ReferenceError(`No storage item with key: \"${key}\"`);
-		return party[key];
-	} catch (err) {
-		console.error(err);
-	}
-}
-
-export async function getParty(): Promise<Party> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party: Party = JSON.parse(partyJSON);
-		return party;
-	} catch (err) {
-		console.error(err);
-		return {};
-	}
-}
-
-export async function getPokemonFromParty(): Promise<Species[]> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party: Party = JSON.parse(partyJSON);
-		const pokemon = Object.entries(party).map(([_, pkmn]) => pkmn);
-		return pokemon;
-	} catch (err) {
-		console.error(err);
-		return [];
-	}
-}
-
-export async function removePokemonFromParty(key: string): Promise<Party> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party: Party = JSON.parse(partyJSON);
-		if (!party[key])
-			throw new ReferenceError(`No storage item with key: \"${key}\"`);
-		console.log(`Removed ${key} from party`);
-		delete party[key];
-		return party;
-	} catch (err) {
-		console.error(err);
-		return {};
-	}
-}
-
-export async function removeAllPokemonFromParty(): Promise<Party> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party: Party = {};
-		AsyncStorage.setItem('party', JSON.stringify(party));
-		console.log('Removed all pokemon from party');
-		return party;
-	} catch (err) {
-		console.log(err);
-		return {};
-	}
-}
-
-async function allowOnlySixInParty(): Promise<boolean> {
-	try {
-		const partyJSON = await AsyncStorage.getItem('party');
-		if (!partyJSON) throw new ReferenceError('No storage item called "party"');
-		const party = JSON.parse(partyJSON);
-		const count = Object.keys(party).length;
-		if (count >= 6) {
-			throw new RangeError('You already have six pokemon in your party');
+	async function insert(value: Species) {
+		try {
+			const party = (await storage.load<Party>({ key: 'party' })) || {};
+			confirmOnlySixInParty();
+			if (party[value.name]) {
+				throw new ReferenceError(
+					`A Pokemon with the name \"${value.name}\" is already in your party`
+				);
+			}
+			party[value.name] = value;
+			await storage.save({ key: 'party', data: party });
+		} catch (err) {
+			console.error(err);
 		}
-		return true;
-	} catch (err) {
-		console.error(err);
-		return false;
 	}
+
+	async function getAll() {
+		try {
+			const party = await storage.load<Party>({ key: 'party' });
+			return Object.entries(party).map(([_, pkmn]) => pkmn);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async function getOne(key: string) {
+		try {
+			const party = await storage.load<Party>({ key: 'party' });
+			if (!party[key]) {
+				throw new ReferenceError(
+					`No pokemon with name \"${key}\" was found in your party`
+				);
+			}
+			return party[key];
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async function removeOne(key: string) {
+		try {
+			const party = await storage.load<Party>({ key: 'party' });
+			if (!party[key]) {
+				throw new ReferenceError(
+					`No pokemon with name \"${key}\" was found in your party`
+				);
+			}
+			delete party[key];
+			await storage.save({ key: 'party', data: party });
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async function removeAll() {
+		await storage.save({ key: 'party', data: {} });
+	}
+
+	async function confirmOnlySixInParty() {
+		try {
+			const party = await storage.load<Party>({ key: 'party' });
+			const count = Object.keys(party).length;
+			if (count >= 6)
+				throw new RangeError('You already have six pokemon in your party');
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	return {
+		insert,
+		getOne,
+		getAll,
+		removeOne,
+		removeAll
+	};
 }
+
+export const party = Party();
