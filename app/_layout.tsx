300a848960e2d4c39a7ea0581f7ec94e922b740e
bradpreston <bap5393@gmail.com>
2023-10-24 07:28:10 -0400

    Add react-query and context to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,24 @@ import '../global.css';
 import { SplashScreen, Stack, Tabs } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { PartyContext } from '../components/context';
 
 export default function Layout() {
 	const [fontsLoaded, fontsError] = useFonts({
 		'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
 		'Roboto-Black': require('../assets/fonts/Roboto-Black.ttf')
+	});
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				cacheTime: 0,
+				staleTime: 0,
+				refetchOnWindowFocus: true
+			}
+		}
 	});
 
 	const onLayoutRootView = useCallback(async () => {
@@ -16,16 +28,21 @@ export default function Layout() {
 		}
 	}, [fontsLoaded, fontsError]);
 
+	const party = useContext(PartyContext);
 	if (!fontsLoaded || fontsError) return null;
 
 	return (
 		<SafeAreaProvider onLayout={onLayoutRootView}>
-			<Stack
-				screenOptions={{
-					title: 'Pokemon'
-				}}>
-				<Stack.Screen name='(pokemon)' />
-			</Stack>
+			<QueryClientProvider client={queryClient}>
+				<PartyContext.Provider value={party}>
+					<Stack
+						screenOptions={{
+							title: 'Pokemon'
+						}}>
+						<Stack.Screen name='(pokemon)' />
+					</Stack>
+				</PartyContext.Provider>
+			</QueryClientProvider>
 		</SafeAreaProvider>
 	);
 }
