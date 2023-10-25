631fa5cd3fe3601226c77bdcbdf62268e000d0e2
bradpreston <bap5393@gmail.com>
2023-10-25 07:00:06 -0400

    Create TypeChip component that returns a chip with a bg color of the type

---
+++ components/TypeChip.tsx
@@ -0,0 +1,68 @@
+import { View, Text } from 'react-native';
+
+type TypeColors = {
+	[key: string]: {
+		bg: string;
+	};
+};
+
+type Props = {
+	type: string;
+};
+
+const typeColors: TypeColors = {
+	fire: {
+		bg: 'bg-rose-600'
+	},
+	water: {
+		bg: 'bg-blue-500'
+	},
+	psychic: {
+		bg: 'bg-indigo-500'
+	},
+	flying: {
+		bg: 'bg-slate-500'
+	},
+	grass: {
+		bg: 'bg-green-500'
+	},
+	poison: {
+		bg: 'bg-pink-500'
+	},
+	normal: {
+		bg: 'bg-stone-500'
+	},
+	electric: {
+		bg: 'bg-yellow-500'
+	},
+	ground: {
+		bg: 'bg-orange-500'
+	},
+	ice: {
+		bg: 'bg-cyan-500'
+	},
+	fighting: {
+		bg: 'bg-yellow-900'
+	},
+	bug: {
+		bg: 'bg-lime-500'
+	},
+	rock: {
+		bg: 'bg-amber-500'
+	},
+	ghost: {
+		bg: 'bg-violet-400'
+	},
+	dragon: {
+		bg: 'bg-emerald-700'
+	}
+};
+
+export default function TypeChip({ type }: Props) {
+	return (
+		<View
+			className={`${typeColors[type]['bg']} w-24 h-8 flex items-center justify-center rounded-2xl`}>
+			<Text className='text-white text-xl capitalize'>{type}</Text>
+		</View>
+	);
+}
