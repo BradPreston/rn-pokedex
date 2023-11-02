5782f19cfdb8dd2dd5a61e03791587d7a58ecc76
bradpreston <bap5393@gmail.com>
2023-11-01 20:26:42 -0400

    Update SecureStore functions to throw proper errors and add capitalFirstLetter function

--- storage/index.ts
+++ storage/index.ts
@@ -5,67 +5,83 @@ type Party = {
 	[key: string]: SimplePokemon;
 };
 
+function capitalFirstLetter(value: string): string {
+	return value.replace(value.charAt(0), value.charAt(0).toUpperCase());
+}
+
 function Party() {
-	async function insert(value: SimplePokemon) {
+	async function insert(value: SimplePokemon): Promise<string> {
 		try {
 			let partyJSON = await SecureStore.getItemAsync('party');
 			if (!partyJSON) partyJSON = JSON.stringify({});
 			const party: Party = JSON.parse(partyJSON);
-			const partyTotal = await confirmOnlySixInParty();
-			if (!partyTotal.lessThanSix) throw new RangeError(partyTotal.err);
+			const partyTotal = Object.keys(party).length;
+			if (partyTotal >= 6)
+				throw new RangeError('You already have six pokemon in your party');
 			if (party[value.name]) {
 				throw new ReferenceError(
-					`A Pokemon with the name \"${value.name}\" is already in your party`
+					`\"${capitalFirstLetter(value.name)}}\" is already in your party`
 				);
 			}
 			party[value.name] = value;
 			await SecureStore.setItemAsync('party', JSON.stringify(party));
+			return `${capitalFirstLetter(value.name)} has been added to your party`;
 		} catch (err) {
-			console.error(err);
+			if (err instanceof ReferenceError) throw new ReferenceError(err.message);
+			if (err instanceof RangeError) throw new RangeError(err.message);
+			throw new Error('An error ocurred');
 		}
 	}
 
-	async function getAll() {
+	async function getAll(): Promise<SimplePokemon[]> {
 		try {
 			let partyJSON = await SecureStore.getItemAsync('party');
 			if (!partyJSON) throw new ReferenceError('No party was found');
 			const party: Party = JSON.parse(partyJSON);
 			return Object.entries(party).map(([_, pkmn]) => pkmn);
 		} catch (err) {
-			console.error(err);
+			if (err instanceof ReferenceError) throw new ReferenceError(err.message);
+			throw new Error('Something went wrong');
 		}
 	}
 
-	async function getOne(key: string) {
+	async function getOne(key: string): Promise<SimplePokemon> {
 		try {
 			const partyJSON = await SecureStore.getItemAsync('party');
 			if (!partyJSON) throw new ReferenceError('No party was found');
 			const party = JSON.parse(partyJSON);
 			if (!party[key]) {
 				throw new ReferenceError(
-					`No pokemon with name \"${key}\" was found in your party`
+					`No pokemon with name \"${capitalFirstLetter(
+						key
+					)}\" was found in your party`
 				);
 			}
 			return party[key];
 		} catch (err) {
-			console.error(err);
+			if (err instanceof ReferenceError) throw new ReferenceError(err.message);
+			throw new Error('Something went wrong');
 		}
 	}
 
-	async function removeOne(key: string) {
+	async function removeOne(key: string): Promise<string> {
 		try {
 			const partyJSON = await SecureStore.getItemAsync('party');
 			if (!partyJSON) throw new ReferenceError('No party was found');
 			const party = JSON.parse(partyJSON);
 			if (!party[key]) {
 				throw new ReferenceError(
-					`No pokemon with name \"${key}\" was found in your party`
+					`No pokemon with name \"${capitalFirstLetter(
+						key
+					)}\" was found in your party`
 				);
 			}
 			delete party[key];
 			await SecureStore.setItemAsync('party', JSON.stringify(party));
+			return `${capitalFirstLetter(key)} was removed from your party`;
 		} catch (err) {
-			console.error(err);
+			if (err instanceof ReferenceError) throw new Error(err.message);
+			throw new Error('Something went wrong');
 		}
 	}
 
@@ -86,7 +102,7 @@ function Party() {
 				lessThanSix: true
 			};
 		} catch (err) {
-			console.error(err);
+			// console.error(err);
 
 			return {
 				err: err instanceof RangeError ? err.message : 'something went wrong',
