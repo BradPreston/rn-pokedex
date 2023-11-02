148b0d1ab0e82e70897a91d30ff20a72cb8dc4ab
bradpreston <bap5393@gmail.com>
2023-11-01 20:21:02 -0400

    Wrap ListItem in Link to make whole item clickable

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -13,22 +13,20 @@ export default memo(function ListItem({ id, name }: Props) {
 	const clientQuery = useQueryClient();
 
 	return (
-		<View className='flex justify-left items-center flex-row gap-5 ml-5'>
-			<Image
-				className='w-16 h-16'
-				source={{
-					uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-				}}
-			/>
-			<Link
-				href={`/pokemon/${id}`}
-				onPress={async () =>
-					await clientQuery.prefetchQuery('pokemonById', () =>
-						getPokemonById(id)
-					)
-				}>
+		<Link
+			href={`/pokemon/${id}`}
+			onPress={async () =>
+				await clientQuery.prefetchQuery('pokemonById', () => getPokemonById(id))
+			}>
+			<View className='flex justify-left items-center flex-row gap-5 pl-5'>
+				<Image
+					className='w-16 h-16'
+					source={{
+						uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+					}}
+				/>
 				<Text className='text-xl capitalize'>{name}</Text>
-			</Link>
-		</View>
+			</View>
+		</Link>
 	);
 });
