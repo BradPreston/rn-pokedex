70848ca9f51ad1ad2f2eba368a21c920b16e8581
bradpreston <bap5393@gmail.com>
2023-11-04 12:38:44 -0400

    Add styles to tab titles

--- app/(pokemon)/_layout.tsx
+++ app/(pokemon)/_layout.tsx
@@ -1,10 +1,57 @@
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Text } from 'react-native';
+import { useState } from 'react';
 
 export default function PokemonLayout() {
+	const [active, setActive] = useState();
 	return (
 		<Tabs screenOptions={{ headerShown: false }}>
-			<Tabs.Screen name='pokemon' />
-			<Tabs.Screen name='party' />
+			<Tabs.Screen
+				name='pokemon'
+				options={{
+					title: 'Pokemon',
+					tabBarLabel: ({ focused, children }) => (
+						<Text
+							className={`${
+								focused ? 'text-pokemon-blue' : 'text-pokemon-grey'
+							}`}>
+							{children}
+						</Text>
+					),
+					tabBarIcon: ({ size, focused }) => (
+						<Text
+							className={`${
+								focused ? 'text-pokemon-blue' : 'text-pokemon-grey'
+							}`}>
+							<Ionicons name='list-outline' size={size} />
+						</Text>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name='party'
+				options={{
+					title: 'Party',
+					tabBarLabel: ({ focused, children }) => (
+						<Text
+							className={`${
+								focused ? 'text-pokemon-blue' : 'text-pokemon-grey'
+							}`}>
+							{children}
+						</Text>
+					),
+
+					tabBarIcon: ({ size, focused }) => (
+						<Text
+							className={`${
+								focused ? 'text-pokemon-blue' : 'text-pokemon-grey'
+							}`}>
+							<Ionicons name='people-outline' size={size} />
+						</Text>
+					)
+				}}
+			/>
 		</Tabs>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
 	presets: [require('nativewind/preset')],
 	content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			'pokemon-grey': 'rgb(156 163 175)',
+			'pokemon-red': '#cc0000',
+			'pokemon-blue': '#3b4cca',
+			'pokemon-yellow': '#ffde00',
+			'pokemon-gold': '#b3a125'
+		}
 	},
 	plugins: []
 };
