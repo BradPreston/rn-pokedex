0f0756fc6d3cc0f80fe9a76325532526ac2d62b3
bradpreston <bap5393@gmail.com>
2023-11-16 07:48:13 -0500

    Clean-up [id].tsx file

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text, Image } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { party } from '@storage';
 import { convertMetersToFeetString, convertToPounds } from '@internal';
 import { useState } from 'react';
@@ -21,44 +21,45 @@ export default function PokemonById() {
 	}>();
 	const [removedPokemon, setRemovedPokemon] = useState(false);
 
-	// const { data: pokemonParty, refetch } = useQuery({
-	// 	queryKey: ['pokemonParty'],
-	// 	queryFn: party.getAll
-	// });
-	// const { data: pokemonParty, refetch } = useGQLQuery<Pokemon[]>({
-	// 	key: 'pokemonParty',
-	// 	query: ALL_POKEMON
-	// });
-	// const addToPartyMutation = useMutation({ ['pokemonParty', party.insert, {
-	// 	onSuccess: () => {
-	// 		setRemovedPokemon(false);
-	// 		refetch();
-	// 	}
-	// });
-	// const removeFromPartyMutation = useMutation('pokemonParty', party.removeOne, {
-	// 	onSuccess: () => {
-	// 		setRemovedPokemon(true);
-	// 		refetch();
-	// 	}
-	// });
+	const { data: pokemonParty, refetch } = useQuery({
+		queryKey: ['pokemonParty'],
+		queryFn: party.getAll
+	});
 
-	// if (addToPartyMutation.status === 'error') {
-	// 	if (addToPartyMutation.error instanceof Error) {
-	// 		toast.error(addToPartyMutation.error.message);
-	// 	}
-	// }
+	const addToPartyMutation = useMutation({
+		mutationKey: ['pokemonParty'],
+		mutationFn: party.insert,
+		onSuccess: () => {
+			setRemovedPokemon(false);
+			refetch();
+		}
+	});
 
-	// if (removeFromPartyMutation.status === 'error') {
-	// 	if (removeFromPartyMutation.error instanceof Error) {
-	// 		toast.error(removeFromPartyMutation.error.message);
-	// 	}
-	// }
+	const removeFromPartyMutation = useMutation({
+		mutationKey: ['pokemonParty'],
+		mutationFn: party.removeOne,
+		onSuccess: () => {
+			setRemovedPokemon(true);
+			refetch();
+		}
+	});
+
+	if (addToPartyMutation.status === 'error') {
+		if (addToPartyMutation.error instanceof Error) {
+			toast.error(addToPartyMutation.error.message);
+		}
+	}
+
+	if (removeFromPartyMutation.status === 'error') {
+		if (removeFromPartyMutation.error instanceof Error) {
+			toast.error(removeFromPartyMutation.error.message);
+		}
+	}
 
 	if (!id) return;
-	const { data: pokemonParty, refetch } = useAllPokemon();
 
 	const numId = parseInt(id);
-	const { data, isLoading, isError, isSuccess, error } = usePokemonById(numId);
+	const { data, isLoading, isError, isSuccess } = usePokemonById(numId);
 
 	if (isLoading) {
 		return (
@@ -69,7 +70,6 @@ export default function PokemonById() {
 	}
 
 	if (isError) {
-		console.log('[id] error:', error);
 		return (
 			<View>
 				<Text>An error ocurred</Text>
@@ -79,19 +79,16 @@ export default function PokemonById() {
 
 	if (isSuccess) {
 		const species = data.species[0];
-		// console.log('pokemon: ', pokemon);
-		const pokemonInfo = species.pokemon.nodes[0];
-		const { id, name } = pokemonInfo;
+		const { id, name, height, weight, types } = species.pokemon.nodes[0];
 
-		const isInParty = pokemonParty?.pokemon?.filter(
-			(pkmn) => pkmn.name === name
-		)[0];
-		// if (addToPartyMutation.status === 'success') {
-		// 	toast.success(addToPartyMutation.data);
-		// }
-		// if (removeFromPartyMutation.status === 'success') {
-		// 	toast.success(removeFromPartyMutation.data);
-		// }
+		const isInParty = pokemonParty?.filter((pkmn) => pkmn.name === name)[0];
+
+		if (addToPartyMutation.status === 'success') {
+			toast.success(addToPartyMutation.data);
+		}
+		if (removeFromPartyMutation.status === 'success') {
+			toast.success(removeFromPartyMutation.data);
+		}
 
 		return (
 			<Container>
@@ -100,7 +97,7 @@ export default function PokemonById() {
 						showModal={showModal}
 						handleCancel={() => setShowModal(false)}
 						handleConfirm={() => {
-							// removeFromPartyMutation.mutate(name);
+							removeFromPartyMutation.mutate(name);
 							setShowModal(false);
 						}}
 					/>
@@ -114,14 +111,12 @@ export default function PokemonById() {
 				<Text className='capitalize text-4xl my-2'>{name}</Text>
 				<View className='flex flex-row justify-between'>
 					<View>
-						<Text>Height: {convertMetersToFeetString(pokemonInfo.height)}</Text>
-						<Text>Weight: {convertToPounds(pokemonInfo.weight)} lbs</Text>
+						<Text>Height: {convertMetersToFeetString(height)}</Text>
+						<Text>Weight: {convertToPounds(weight)} lbs</Text>
 					</View>
 					<View className='flex flex-row gap-2'>
-						<TypeChip type={pokemonInfo.types[0].type.name} />
-						{pokemonInfo.types[1] && (
-							<TypeChip type={pokemonInfo.types[1].type.name} />
-						)}
+						<TypeChip type={types[0].type.name} />
+						{types[1] && <TypeChip type={types[1].type.name} />}
 					</View>
 				</View>
 				<Text className='mt-5'>
@@ -131,27 +126,14 @@ export default function PokemonById() {
 				</Text>
 
 				<View className='flex items-center mt-5'>
-					{isInParty && !removedPokemon && (
+					{isInParty && !removedPokemon ? (
 						<PartyButton
 							removeFromParty={() => setShowModal(true)}
 							styles='bg-pokemon-red rounded-md flex items-center justify-center w-full py-3'
 						/>
-					)}
-					{isInParty && removedPokemon && (
+					) : (
 						<PartyButton
-							// addToParty={() => addToPartyMutation.mutate({ id, name })}
-							styles='bg-pokemon-slate rounded-md flex items-center justify-center w-full py-3'
-						/>
-					)}
-					{!isInParty && !removedPokemon && (
-						<PartyButton
-							// addToParty={() => addToPartyMutation.mutate({ id, name })}
-							styles='bg-pokemon-slate rounded-md flex items-center justify-center w-full py-3'
-						/>
-					)}
-					{!isInParty && removedPokemon && (
-						<PartyButton
-							// addToParty={() => addToPartyMutation.mutate({ id, name })}
+							addToParty={() => addToPartyMutation.mutate({ id, name })}
 							styles='bg-pokemon-slate rounded-md flex items-center justify-center w-full py-3'
 						/>
 					)}
