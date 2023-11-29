a0aeb43828b7d1e2811b151c631003b383f7bc51
bradpreston <bap5393@gmail.com>
2023-11-28 21:34:15 -0500

    Remove colors from tailwind.config and replace with tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	presets: [require('nativewind/preset')],
-	content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*{js,jsx,ts,tsx}'],
+	content: [
+		'./app/**/*.{js,jsx,ts,tsx}',
+		'./components/**/*{js,jsx,ts,tsx}',
+		'./internal/**/*{js,jsx,ts,tsx}'
+	],
 	theme: {
-		extend: {},
-		colors: {
-			'pokemon-white': '#ffffff',
-			'pokemon-black': '#333333',
-			'pokemon-slate': 'rgb(51 65 85)',
-			'pokemon-grey': 'rgb(156 163 175)',
-			'pokemon-lightgrey': 'rgb(209 213 219)',
-			'pokemon-red': '#cc0000',
-			'pokemon-blue': '#3b4cca',
-			'pokemon-yellow': '#ffde00',
-			'pokemon-gold': '#b3a125'
-		}
+		extend: {}
 	},
 	plugins: []
 };
