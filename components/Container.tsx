81c2b9c038a0f03ef616a4c97b0b0e82c586dc5b
bradpreston <bap5393@gmail.com>
2023-11-28 21:37:35 -0500

    Update container to use full height of screen

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 
 export default function Container({ children }: PropsWithChildren) {
 	return (
-		<View className='flex flex-col'>
+		<View className='flex-col relative h-full'>
 			<View className='w-11/12 self-center'>{children}</View>
 		</View>
 	);
