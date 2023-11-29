fb86c809886dfc8b80ea3aead143957718e5790c
bradpreston <bap5393@gmail.com>
2023-11-28 21:36:53 -0500

    Cleanup types file

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,4 @@
-// export type Pokemon = {
-// 	__typename: string;
-// 	id: number;
-// 	name: string;
-// 	height: number;
-// 	weight: number;
-// 	abilities: {
-// 		nodes: Ability[];
-// 	};
-// 	stats: Stat[];
-// 	types: Type[];
-// 	pokemonSprites: Sprite[];
-// };
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 export type PokemonArr = {
 	pokemon: Pokemon[];
@@ -39,63 +27,13 @@ type Type = {
 };
 
 type FlavorText = {
-	flavor_text: Description[];
-};
-
-type Description = {
 	description: string;
 };
 
-// export type SpeciesArr = {
-// 	species: Species[];
-// };
-
-// export type Species = {
-// 	name: string;
-// 	is_legendary: boolean;
-// 	is_mythical: boolean;
-// 	pokemon: {
-// 		nodes: Pokemon[];
-// 	};
-// 	habitat: {
-// 		name: string;
-// 	};
-// 	flavorText: FlavorText[];
-// };
-
-// export type SimplePokemonArr = {
-// 	pokemon: SimplePokemon[];
-// };
-
-// export type SimplePokemon = {
-// 	id: number;
-// 	name: string;
-// };
-
-// type Ability = {
-// 	ability: {
-// 		name: string;
-// 	};
-// };
-
-// type Stat = {
-// 	base_stat: number;
-// 	stat: {
-// 		name: string;
-// 	};
-// };
-
-// // type Type = {
-// // 	slot: number;
-// // 	type: {
-// // 		name: string;
-// // 	};
-// // };
-
-// type Sprite = {
-// 	sprites: string;
-// };
-
-// type FlavorText = {
-// 	flavor_text: string;
-// };
+export type TypeColors = {
+	[key: string]: {
+		chipBackground: string;
+		cardBackground: string;
+		icon: IconDefinition;
+	};
+};
