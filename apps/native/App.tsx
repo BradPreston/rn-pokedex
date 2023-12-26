a03bcc42b5522a6f7aff12f8ba5969c21540ab35
bradpreston <bap5393@gmail.com>
2023-12-26 17:32:27 -0500

    Add custom query provider to App.tsx

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,10 +1,34 @@
 import './global.css';
-import { Text, View } from 'react-native';
+import { Text, View, LogBox } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import { Provider } from '@repo/query';
+import { SplashScreen } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+LogBox.ignoreAllLogs();
 
 export default function Native() {
+	const [fontsLoaded, fontsError] = useFonts({
+		'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+		'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf')
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontsError) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, fontsError]);
+
+	if (!fontsLoaded || fontsError) return null;
+
 	return (
-		<View className='items-center justify-center w-full h-full'>
-			<Text className='text-red-500 text-4xl font-bold'>Hello</Text>
-		</View>
+		<SafeAreaProvider onLayout={onLayoutRootView}>
+			<Provider>
+				<View className='items-center justify-center w-full h-full'>
+					<Text className='text-red-500 text-4xl font-bold'>Hello</Text>
+				</View>
+			</Provider>
+		</SafeAreaProvider>
 	);
 }
