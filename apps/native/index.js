f8c1ef479e49d74d19ec43cf857b5a1bda86aeb2
bradpreston <bap5393@gmail.com>
2024-01-07 21:11:50 -0500

    Add Provider to index.js

--- apps/native/index.js
+++ apps/native/index.js
@@ -1,6 +1,14 @@
-import { registerRootComponent } from "expo";
+import { registerRootComponent } from 'expo';
+import { Provider } from '@repo/query';
+import Native from './App';
 
-import App from "./App";
+function App() {
+	return (
+		<Provider>
+			<Native />
+		</Provider>
+	);
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
