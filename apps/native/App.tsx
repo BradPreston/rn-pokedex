ea031f50437169547281b63f162dffa2333072fc
bradpreston <bap5393@gmail.com>
2024-01-09 16:36:45 -0500

    Fix fonts not loading and show all pokemon in native app

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,52 +1,41 @@
 import './global.css';
-import { View, LogBox } from 'react-native';
-import * as Font from 'expo-font';
-import { useCallback, useEffect, useState } from 'react';
-import { PokemonQuery, GET_POKEMON_BY_ID, GET_ALL_POKEMON } from '@repo/query';
+import { View, LogBox, FlatList, ListRenderItem } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import { PokemonQuery, GET_ALL_POKEMON } from '@repo/query';
 import { SplashScreen } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Paragraph, Heading, Card } from '@repo/ui';
-import { PokemonArr } from '@repo/types';
+import { Pokemon, PokemonArr } from '@repo/types';
 
 LogBox.ignoreAllLogs();
 SplashScreen.preventAutoHideAsync();
 
 export default function Native() {
-	const [appIsReady, setAppIsReady] = useState(false);
+	const { data } = PokemonQuery<PokemonArr>('allPokemon', GET_ALL_POKEMON);
 
-	const { data } = PokemonQuery<PokemonArr>(
-		'pokemonById',
-		GET_POKEMON_BY_ID,
-		1
+	const [fontsLoaded] = useFonts({
+		Roboto: require('./assets/fonts/Roboto-Regular.ttf'),
+		'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf')
+	});
+
+	const pokeballImage = require('./assets/white-pokeball-bg.png');
+
+	const renderItem = useCallback<ListRenderItem<Pokemon>>(
+		({ item }) => <Card pokemon={item} image={pokeballImage} />,
+		[]
 	);
 
-	useEffect(() => {
-		async function prepare() {
-			try {
-				await Font.loadAsync('./assets/fonts/Roboto-Regular.ttf');
-				await Font.loadAsync('./assets/fonts/Roboto-Black.ttf');
-
-				await new Promise((resolve) => setTimeout(resolve, 2000));
-			} catch (err) {
-				console.warn(err);
-			} finally {
-				setAppIsReady(true);
-			}
-		}
-
-		prepare();
-	}, []);
-
 	const onLayoutRootView = useCallback(async () => {
-		if (appIsReady) await SplashScreen.hideAsync();
-	}, [appIsReady]);
+		if (fontsLoaded) await SplashScreen.hideAsync();
+	}, [fontsLoaded]);
 
-	if (!appIsReady) return;
+	if (!fontsLoaded) return;
 	if (!data) return;
 
 	return (
 		<SafeAreaProvider onLayout={onLayoutRootView}>
-			<View className='items-center justify-center w-full h-full'>
+			<View className='items-center justify-center w-full h-full px-5'>
 				<Heading text='heading 1' type='H1' />
 				<Heading text='heading 2' type='H2' />
 				<Heading text='heading 3' type='H3' />
@@ -55,10 +44,14 @@ export default function Native() {
 				<Heading text='heading 6' type='H6' />
 				<Paragraph text='hello there' />
 
-				<Card
-					pokemon={data.pokemon[0]}
-					image={require('./assets/favicon.png')}
-				/>
+				<View className='w-full'>
+					<FlatList
+						data={data.pokemon}
+						renderItem={renderItem}
+						keyExtractor={(item) => item.name}
+						showsVerticalScrollIndicator={false}
+					/>
+				</View>
 			</View>
 		</SafeAreaProvider>
 	);
