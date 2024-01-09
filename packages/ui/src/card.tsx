86b4bca631ab8d297f29bdb54d8a942dba8536e1
bradpreston <bap5393@gmail.com>
2024-01-09 16:36:11 -0500

    Update card styles in ui package

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -9,29 +9,33 @@ import {
 } from 'react-native';
 import { Pokemon } from '@repo/types';
 import typeColors from './typeColors';
+import { TypeChip } from './typeChip';
 
 function createDynamicStylesheet(typeName: string) {
 	return StyleSheet.create({
 		box: {
-			backgroundColor: typeColors[typeName]['chipBackground'],
-			marginTop: 12, // mt-12
-			borderRadius: 12 // rounded-xl
+			backgroundColor: typeColors[typeName]['cardBackground'],
+			marginTop: 48, // mt-12
+			borderRadius: 12, // rounded-xl
+			width: '100%'
 		},
 		wrapper: {
-			width: '100%',
 			overflow: 'hidden',
+			width: '100%',
 			padding: 20 // p-5
 		},
 		number: {
-			fontWeight: 'bold',
-			color: 'rgb(55 65 81)' // text-gray-700
+			color: 'rgb(55 65 81)', // text-gray-700
+			fontFamily: 'Roboto-Black',
+			fontSize: 16
 		},
 		name: {
 			fontSize: 36, // text-4xl
 			lineHeight: 40, // text-4xl
 			textTransform: 'capitalize',
 			color: '#ffffff', // text-white
-			paddingVertical: 8 // py-2
+			paddingVertical: 8, // py-2
+			fontFamily: 'Roboto-Black'
 		},
 		types: {
 			flexDirection: 'row', // flex-row
@@ -54,6 +58,11 @@ function createDynamicStylesheet(typeName: string) {
 			position: 'absolute',
 			right: 8, // right-2
 			top: -32 // -top-8
+		},
+		pokemonImage: {
+			objectFit: 'contain',
+			height: 160, // h-40
+			aspectRatio: '1/1'
 		}
 	});
 }
@@ -70,13 +79,13 @@ export function Card({ pokemon, image }: CardProps) {
 
 	return (
 		<View style={style.box}>
-			<Pressable>
+			<Pressable style={{ width: '100%' }}>
 				<View style={style.wrapper}>
-					<Text style={style.number}>#{id}</Text>
+					<Text style={style.number}>#{id.toString().padStart(3, '0')}</Text>
 					<Text style={style.name}>{name}</Text>
 					<View style={style.types}>
 						{types.map((type, index) => (
-							<Text key={type.type.name + index}>{type.type.name}</Text>
+							<TypeChip key={type.type.name + index} type={type.type.name} />
 						))}
 					</View>
 
@@ -84,7 +93,14 @@ export function Card({ pokemon, image }: CardProps) {
 						<Image style={style.pokeballImage} source={image} />
 					</View>
 				</View>
-				<View style={style.pokemonImageWrapper}>{/* pokemon image */}</View>
+				<View style={style.pokemonImageWrapper}>
+					<Image
+						style={style.pokemonImage}
+						source={{
+							uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+						}}
+					/>
+				</View>
 			</Pressable>
 		</View>
 	);
