42643047619f0462dca5eb73142bf685ff7c41d2
bradpreston <bap5393@gmail.com>
2024-01-13 15:32:06 -0500

    Add provider and fonts to web app layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,27 @@
+import { Provider } from '@repo/query';
+import localFont from 'next/font/local';
+
+const roboto = localFont({
+	src: [
+		{
+			path: '../public/fonts/Roboto-Black.ttf'
+		},
+		{
+			path: '../public/fonts/Roboto-Regular.ttf'
+		}
+	]
+});
+
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body>
+				<Provider>{children}</Provider>
+			</body>
+		</html>
+	);
 }
