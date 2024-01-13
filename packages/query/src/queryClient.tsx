c09b98c7b5103fe0b7bde52003408185b5dd9bdb
bradpreston <bap5393@gmail.com>
2024-01-13 15:33:28 -0500

    Add use client to queryClient component

--- packages/query/src/queryClient.tsx
+++ packages/query/src/queryClient.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { QueryClient } from '@tanstack/react-query';
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
