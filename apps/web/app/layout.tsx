05cb007d142ab918bb0142267a7235993b102790
bradpreston <bap5393@gmail.com>
2024-01-13 17:51:06 -0500

    Remove custom font from layout on web app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,4 @@
 import { Provider } from '@repo/query';
-import localFont from 'next/font/local';
-
-const roboto = localFont({
-	src: [
-		{
-			path: '../public/fonts/Roboto-Black.ttf'
-		},
-		{
-			path: '../public/fonts/Roboto-Regular.ttf'
-		}
-	]
-});
 
 export default function RootLayout({
 	children
