3dfa3777bcd929431c9b15a5151e3a67ee430201
bradpreston <bap5393@gmail.com>
2024-01-22 16:54:06 -0500

    Export typeColors and TypeChip from package/ui

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,5 @@ export { Button, type ButtonProps } from './button';
 export { Paragraph } from './paragraph';
 export { Heading } from './heading';
 export { Card } from './card';
+export { default as TypeColors } from './typeColors';
+export { TypeChip } from './typeChip';
