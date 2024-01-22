b7684a3fb548671ba359e27c709b9d5d82324b96
bradpreston <bap5393@gmail.com>
2024-01-22 16:56:01 -0500

    Add light styles to package/ui styles

--- packages/ui/src/styles.ts
+++ packages/ui/src/styles.ts
@@ -4,5 +4,9 @@ export const styles = StyleSheet.create({
 	default: {
 		color: 'rgb(51, 65, 85)', // text-slate-700
 		fontSize: 16 // text-base
+	},
+	light: {
+		color: 'rgb(255, 255, 255)', // text-white
+		fontSize: 16 // font-base
 	}
 });
