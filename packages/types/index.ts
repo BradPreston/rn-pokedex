219e539a0420753ddbd22b4bf91656fa382851dd
bradpreston <bap5393@gmail.com>
2024-01-22 16:55:17 -0500

    Update types after codegen

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -5,29 +5,77 @@ export type PokemonArr = {
 };
 
 export type Pokemon = {
-	id: number;
-	name: string;
-	is_legendary?: boolean;
-	details: {
-		nodes: Details[];
-	};
-	flavor_text: FlavorText[];
+	details: Details[];
+	efficacies: Efficacy[];
+	evolution_chain: EvolutionChain[];
+	pokemon_stat: Stat[];
+	pokemon_type: Type[];
+	species: Species[];
 };
 
 type Details = {
+	name: string;
+	id: number;
 	height: number;
 	weight: number;
-	types: Type[];
+	abilities: Ability[];
 };
 
-type Type = {
+type Ability = {
+	ability: {
+		name: string;
+		description: FlavorText[];
+	};
+};
+
+type Efficacy = {
+	target_type: {
+		id: number;
+		name: string;
+		type_efficacy: TypeEfficacy[];
+	};
+};
+
+type TypeEfficacy = {
+	damage_factor: number;
 	type: {
 		name: string;
 	};
 };
 
+type EvolutionChain = {
+	evolution: Evolution[];
+};
+
+type Evolution = {
+	name: string;
+	id: number;
+};
+
+type Stat = {
+	base_stat: number;
+	stat: {
+		name: string;
+	};
+};
+
+type Type = {
+	type: {
+		name: string;
+		id: number;
+	};
+};
+
+type Species = {
+	description: Description[];
+};
+
+type Description = {
+	flavor_text: string;
+};
+
 type FlavorText = {
-	description: string;
+	flavor_text: string;
 };
 
 export type TypeColors = {
