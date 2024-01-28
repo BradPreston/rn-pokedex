eca4ff892c3cd4522e9ea401b1276b0f840beac4
bradpreston <bap5393@gmail.com>
2024-01-28 15:10:15 -0500

    App working again on web/iOS/Android

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -3,18 +3,35 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 // 1. Enable CSS for Expo.
 const config = getDefaultConfig(__dirname, {
-	isCSSEnabled: true
+	isCSSEnabled: true,
+	transformer: {
+		// ...transformer,
+		babelTransformerPath: require.resolve('react-native-svg-transformer'),
+		},
+		// resolver: {
+		// 	// ...resolver,
+		// 	disableHierarchicalLookup: true,
+		// 	assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+		// 	sourceExts: ['svg'],
+		// 	nodeModulesPath: [path.resolve(projectRoot, 'node_modules'), path.resolve(workspaceRoot, 'node_modules')],
+		// 	}
 });
+
+
 
 // This is not needed for NativeWind, it is configuration for Metro to understand monorepos
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 config.watchFolders = [workspaceRoot];
-config.resolver.disableHierarchicalLookup = true;
-config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, 'node_modules'),
-	path.resolve(workspaceRoot, 'node_modules')
-];
+// config.resolver.disableHierarchicalLookup = true;
+// config.resolver.nodeModulesPaths = [
+// 	path.resolve(projectRoot, 'node_modules'),
+// 	path.resolve(workspaceRoot, 'node_modules')
+// ];
+
+// config.
+	
+// 	config.
 
 // 2. Enable NativeWind
 const { withNativeWind } = require('nativewind/metro');
--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,18 +1,24 @@
-import '../global.css';
+import '../../global.css';
 import { LogBox, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import { useCallback } from 'react';
 import { Provider } from '@repo/query';
 import { SplashScreen, Stack, Slot } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 LogBox.ignoreAllLogs();
 SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
 	const [fontsLoaded] = useFonts({
-		Roboto: require('../assets/fonts/Roboto-Regular.ttf'),
-		'Roboto-Black': require('../assets/fonts/Roboto-Black.ttf')
+		...Ionicons.font,
+    ...MaterialCommunityIcons.font,
+    ...FontAwesome.font,
+		Roboto: require('../../assets/fonts/Roboto-Regular.ttf'),
+		'Roboto-Black': require('../../assets/fonts/Roboto-Black.ttf')
 	});
 
 	const onLayoutRootView = useCallback(async () => {
