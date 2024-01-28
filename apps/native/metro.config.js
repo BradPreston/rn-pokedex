e7fd7220ab1c70816b612d7a9ccff1866c63f936
bradpreston <bap5393@gmail.com>
2024-01-28 15:14:03 -0500

    Cleanup metro.config - disableHierarchicalLookup commented out in order to work currently

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -5,33 +5,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname, {
 	isCSSEnabled: true,
 	transformer: {
-		// ...transformer,
-		babelTransformerPath: require.resolve('react-native-svg-transformer'),
-		},
-		// resolver: {
-		// 	// ...resolver,
-		// 	disableHierarchicalLookup: true,
-		// 	assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-		// 	sourceExts: ['svg'],
-		// 	nodeModulesPath: [path.resolve(projectRoot, 'node_modules'), path.resolve(workspaceRoot, 'node_modules')],
-		// 	}
+		babelTransformerPath: require.resolve('react-native-svg-transformer')
+	}
 });
-
-
 
 // This is not needed for NativeWind, it is configuration for Metro to understand monorepos
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 config.watchFolders = [workspaceRoot];
 // config.resolver.disableHierarchicalLookup = true;
-// config.resolver.nodeModulesPaths = [
-// 	path.resolve(projectRoot, 'node_modules'),
-// 	path.resolve(workspaceRoot, 'node_modules')
-// ];
-
-// config.
-	
-// 	config.
 
 // 2. Enable NativeWind
 const { withNativeWind } = require('nativewind/metro');
