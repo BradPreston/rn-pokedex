617390d0d4c71ace5c8e0194f50669239e3b9833
brad preston <bap5393@gmail.com>
2024-02-04 12:11:26 -0500

    Load very simple data on [id] page with spinner while fetching

--- apps/native/src/app/pokemon/[id].tsx
+++ apps/native/src/app/pokemon/[id].tsx
@@ -1,5 +1,42 @@
-import { Text, View } from "react-native";
+import { GET_POKEMON_BY_ID, PokemonQuery } from '@repo/query';
+import { Pokemon } from '@repo/types';
+import { LoadingSpinner } from '@repo/ui/spinner';
+import { useLocalSearchParams } from 'expo-router';
+import { Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default function PokemonById() {
-  return <View><Text>ID</Text></View>
+	// grab the pokemon id from the url
+	const { id } = useLocalSearchParams<{
+		id: string;
+	}>();
+
+	// if no id is found, early return
+	if (!id) return;
+
+	// grab the pokemon data by id
+	const { data, isFetching } = PokemonQuery<Pokemon>(
+		'pokemonById',
+		GET_POKEMON_BY_ID,
+		parseInt(id)
+	);
+
+	// if no data is found, early return
+	if (!data) return;
+
+	// if the page is loading data, show a spinner
+	if (isFetching) return <LoadingSpinner />;
+
+	// else show the pokemon details
+	return (
+		<View>
+			<Text>{data.details[0].name}</Text>
+			<Image
+				className='h-40 aspect-square'
+				source={{
+					uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+				}}
+			/>
+			<Text></Text>
+		</View>
+	);
 }
