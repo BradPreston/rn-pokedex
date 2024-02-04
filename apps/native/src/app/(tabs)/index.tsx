9baca60c213342ba56bb30f719d5192f98e7225c
brad preston <bap5393@gmail.com>
2024-02-04 12:12:16 -0500

    Move early return higher in (pokemon)/index and add horizontal padding to view

--- apps/native/src/app/(tabs)/index.tsx
+++ apps/native/src/app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import { GET_ALL_POKEMON, PokemonQuery } from '@repo/query';
 import { AllPokemon } from '@repo/types';
+import { LoadingSpinner } from '@repo/ui';
 import { Card } from '@repo/ui';
 import { FlatList, View } from 'react-native';
 
 export default function Page() {
+	// grab the data for all pokemon
 	const { data } = PokemonQuery<AllPokemon>('allPokemon', GET_ALL_POKEMON);
-
+	// if no data is found, early return
+	if (!data) return;
+	// load the pokeballImage for the card background
 	const pokeballImage = require('../../../assets/white-pokeball-bg.png');
 
-	if (!data) return;
-
 	return (
-		<View>
+		<View className='px-4'>
 			<FlatList
 				data={data.pokemon}
 				renderItem={({ item }) => <Card pokemon={item} image={pokeballImage} />}
