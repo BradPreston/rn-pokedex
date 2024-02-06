2df57608a22215866eb3aace570b227d2a84c29b
brad preston <bap5393@gmail.com>
2024-02-06 14:51:08 -0500

    Update Paragraph component to use Tailwind styles. Remove rn styles from Styles package

--- packages/ui/src/styles.ts
+++ packages/ui/src/styles.ts
@@ -1,16 +1,9 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-	default: {
-		color: 'rgb(51, 65, 85)', // text-slate-700
-		fontSize: 16 // text-base
-	},
-	light: {
-		color: 'rgb(255, 255, 255)', // text-white
-		fontSize: 16 // font-base
-	}
-});
-
+/* 
+Defaults for UI components
+------------------------------------
+Reset spacing, color, etc to allow control where the components are used.
+*/
 export const twStyles = {
-	heading: 'text-slate-700 leading-none my-0'
+	heading: 'text-slate-700 leading-none my-0',
+	paragraph: 'text-slate-700 text-base my-0 leading-none'
 };
