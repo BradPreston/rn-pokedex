ea34ad36909efa8583643b239d55c51de2dad7d3
brad preston <bap5393@gmail.com>
2024-02-06 15:33:07 -0500

    Add option for text shadows in Heading and Paragraph packages

--- packages/ui/src/heading.tsx
+++ packages/ui/src/heading.tsx
@@ -8,22 +8,66 @@ type HeadingProps = {
 	text: string;
 	type: Heading;
 	styles?: string;
+	shadow?: boolean;
 };
 
-export function Heading({ text, type, styles }: HeadingProps) {
+export function Heading({ text, type, styles, shadow }: HeadingProps) {
+	// create drop shadow (Nativewind does not support drop shadows)
+	const dropShadow = {
+		textShadowColor: 'rgba(33, 33, 33, .75)',
+		textShadowOffset: { width: 2, height: 2 },
+		textShadowRadius: 5
+	};
+
 	switch (type) {
 		case 'H1':
-			return <H1 className={`${twStyles.heading} ${styles}`}>{text}</H1>;
+			return (
+				<H1
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H1>
+			);
 		case 'H2':
-			return <H2 className={`${twStyles.heading} ${styles}`}>{text}</H2>;
+			return (
+				<H2
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H2>
+			);
 		case 'H3':
-			return <H3 className={`${twStyles.heading} ${styles}`}>{text}</H3>;
+			return (
+				<H3
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H3>
+			);
 		case 'H4':
-			return <H4 className={`${twStyles.heading} ${styles}`}>{text}</H4>;
+			return (
+				<H4
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H4>
+			);
 		case 'H5':
-			return <H5 className={`${twStyles.heading} ${styles}`}>{text}</H5>;
+			return (
+				<H5
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H5>
+			);
 		case 'H6':
-			return <H6 className={`${twStyles.heading} ${styles}`}>{text}</H6>;
+			return (
+				<H6
+					className={`${twStyles.heading} ${styles}`}
+					style={shadow ? { ...dropShadow } : null}>
+					{text}
+				</H6>
+			);
 		default:
 			throw new Error('improper heading type');
 	}
--- packages/ui/src/paragraph.tsx
+++ packages/ui/src/paragraph.tsx
@@ -5,8 +5,22 @@ import React from 'react';
 type ParagraphProps = {
 	text: string;
 	styles?: string;
+	shadow?: boolean;
 };
 
-export function Paragraph({ text, styles }: ParagraphProps) {
-	return <P className={`${twStyles.paragraph} ${styles}`}>{text}</P>;
+export function Paragraph({ text, styles, shadow }: ParagraphProps) {
+	// create drop shadow (Nativewind does not support drop shadows)
+	const dropShadow = {
+		textShadowColor: 'rgba(33, 33, 33, .75)',
+		textShadowOffset: { width: 2, height: 2 },
+		textShadowRadius: 5
+	};
+
+	return (
+		<P
+			className={`${twStyles.paragraph} ${styles}`}
+			style={shadow ? { ...dropShadow } : null}>
+			{text}
+		</P>
+	);
 }
